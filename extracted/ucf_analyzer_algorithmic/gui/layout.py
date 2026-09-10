60d56220dd64012fd261355442ab40c90a9929cc
EricAqib123 <111480435+EricAqib123@users.noreply.github.com>
2026-09-11 04:45:42 +0500

    Refactor layout.py for improved code structure

--- extracted/ucf_analyzer_algorithmic/gui/layout.py
+++ extracted/ucf_analyzer_algorithmic/gui/layout.py
@@ -23,7 +23,7 @@ PAGES = (
 def configure_page():
     st.set_page_config(
         page_title="AI-Based Algorithmic Analyzer (UCF Model)",
-        page_icon="str(LOGO_PATH2)"
+        page_icon=str(LOGO_PATH2)
         layout="wide",
     )
 
